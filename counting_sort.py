2dbd2e032ff4505e35a06ae23e04a714f11d1176
Boyko <boyko11@gmail.com>
2020-01-26 13:34:59 -0500

    rod_cut and count_sort

---
+++ counting_sort.py
@@ -0,0 +1,23 @@
+
+a = [8, 2, 3, 5]
+
+min_a = min(a)
+max_a = max(a)
+
+count_a = [0 for i in range(max_a - min_a + 1)]
+
+for i in range(len(a)):
+    count_a_index = a[i] - min_a
+    count_a[ count_a_index ] = count_a[ count_a_index ] + 1
+
+print(count_a)
+
+sorted_a = []
+count_a_index = 0
+for count in count_a:
+    for i in range(count):
+        sorted_a.append(count_a_index + min_a)
+    count_a_index += 1
+
+print(sorted_a)
+
